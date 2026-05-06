cff837f863d3120178ac60e0121e8b4fa7cf7d34
Gilbert Jesse <jesse.vickers70@gmail.com>
2026-05-06 09:52:08 +0100

    Removed unused variables field names

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -11,12 +11,12 @@ input_file = open('./eva-data.json', 'r', encoding='ascii')
 output_file = open('./eva-data.csv','w', encoding='utf-8')
 graph_file = './cumulative_eva_graph.png'
 
-fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
+
 
 data=[]
 
 
-for i in range(375):
+for _ in range(375):
     line=input_file.readline()
     print(line)
     data.append(json.loads(line[1:-1]))
